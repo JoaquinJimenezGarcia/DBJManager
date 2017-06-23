599b3c2206aff8f78fae9f02ba13f8856afe748a
Joaquin Jimenez Garcia <garcia.jjimenez@gmail.com>
2017-06-23 02:51:18 +0200

    - Sistema de ventanas

---
+++ src/me/jojigarcia/model/Conexion.java
@@ -0,0 +1,135 @@
+package me.jojigarcia.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Created by garci on 23/06/2017.
+ */
+public class Conexion {
+    private Connection conexion = null;
+    private String nombre;
+    private String user;
+    private Boolean conectado = false;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public Boolean getConectado() {
+        return conectado;
+    }
+
+    public Boolean conectar(String user, String passwd, String db) throws SQLException {
+        String servidor = "jdbc:mysql://localhost:3306/" + db;
+        conexion = DriverManager.getConnection(servidor,user,passwd);
+        conexion.setAutoCommit(false);
+
+        this.nombre = db;
+        this.user = user;
+
+        this.conectado = true;
+
+        return true;
+    }
+
+    public void cerrar() throws SQLException{
+        if (conexion != null){
+            conexion.close();
+            this.conectado = false;
+            System.out.println("Desconectado de la base de datos con éxito.");
+        }
+    }
+
+    public void crearBaseDatos(String nombreDB) throws SQLException{
+        final String QUERY = "CREATE DATABASE " + nombreDB;
+        PreparedStatement db = null;
+
+        try {
+            db = conexion.prepareStatement(QUERY);
+            db.executeUpdate();
+
+            conexion.commit();
+            System.out.println("Base de datos: " + nombreDB + " creada con éxito.");
+        } catch (SQLException e){
+            System.out.println("Ha ocurrido un error al crear la base de datos.");
+            conexion.rollback();
+        } finally {
+            if (db!=null){
+                db.close();
+            }
+        }
+    }
+
+    /*public void crearTabla() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        final String QUERY;
+        PreparedStatement db = null;
+        int atributos;
+        String nombre;
+        String [] listaAtributos;
+
+        System.out.println("Nombre de la tabla: ");
+        nombre = input.next();
+
+        System.out.println("Cuántos atributos va a tener?");
+        atributos = input.nextInt();
+        listaAtributos = new String[atributos];
+
+        QUERY = "Create TABLE " + nombre + " ";
+
+        for (int i = 0; i <= atributos; i++) {
+            System.out.println("Atributo " + (i+1) + " :");
+            listaAtributos[i] = input.next();
+        }
+
+
+        for (int i = 0; i < listaAtributos.length ; i++) {
+            String nombreAtr;
+
+            System.out.println("Introduzca nombre del parametro: ");
+            nombreAtr = input.next();
+            QUERY = QUERY + " " + nombre;
+        }
+
+        try {
+            db = conexion.prepareStatement(QUERY);
+            db.executeUpdate();
+
+            conexion.commit();
+            System.out.println("Tabla creada con éxito.");
+        } catch (SQLException e) {
+            System.out.println("Ha habido un error intentando añadir la tabla");
+            conexion.rollback();
+        } finally {
+            if (db!=null){
+                db.close();
+            }
+        }
+    }*/
+
+    public void cambiarBaseDatos(String nombreDB) throws SQLException{
+        final String query = "USE " + nombreDB;
+        PreparedStatement cambio = null;
+
+        try{
+            cambio = conexion.prepareStatement(query);
+            cambio.executeUpdate();
+
+            System.out.println("Cambio de base de datos realizado con éxito.");
+            this.nombre = nombreDB;
+        } catch (SQLException e) {
+            System.out.println("Ha ocurrido un error durante el cambio de base de datos.");
+        } finally {
+            if (cambio!=null){
+                cambio.close();
+            }
+        }
+    }
+}
--- src/me/jojigarcia/view/Controls.java
+++ src/me/jojigarcia/view/Controls.java
@@ -1,44 +1,68 @@
 package me.jojigarcia.view;
 
+import me.jojigarcia.model.Conexion;
 import me.jojigarcia.model.Email;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 /**
  * Created by joaquinjimenezgarcia on 29/5/17.
  */
 public class Controls extends JPanel {
-    JLabel emailLbl;
-    JTextField emailTxtLbl;
+    JLabel usuarioLbl;
+    JTextField usuarioTxtLbl;
+    JLabel passLbl;
+    JTextField passTxtLbl;
+    JLabel ddbbLbl;
+    JTextField ddbbTxtLbl;
     JButton validateBtn;
 
     public Controls(){
-        emailLbl = new JLabel("Email: ");
-        emailTxtLbl = new JTextField(18);
+        usuarioLbl = new JLabel("Usuario: ");
+        usuarioTxtLbl = new JTextField(18);
+        passLbl = new JLabel("Contraseña: ");
+        passTxtLbl = new JTextField(18);
+        ddbbLbl = new JLabel("Base de Datos: ");
+        ddbbTxtLbl = new JTextField(18);
+
+
         validateBtn = new JButton("Validate");
 
         validateBtn.addActionListener(new ValidateAction());
 
-        add(emailLbl);
-        add(emailTxtLbl);
+        add(usuarioLbl);
+        add(usuarioTxtLbl);
+        add(passLbl);
+        add(passTxtLbl);
+        add(ddbbLbl);
+        add(ddbbTxtLbl);
         add(validateBtn);
     }
 
     public class ValidateAction implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            Email email = new Email(emailTxtLbl.getText());
+            Conexion conexion = new Conexion();
 
-            if (email.validate()){
+            try {
+                if (conexion.conectar(usuarioTxtLbl.getText(), passTxtLbl.getText(), ddbbTxtLbl.getText())){
+                    JOptionPane.showMessageDialog(null,
+                            "Welcome to your database " + ddbbTxtLbl.getText(),
+                            "Conection done",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(null,
+                            "Compruebe que ha insertado bien sus datos.",
+                            "Error",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+            } catch (SQLException x) {
+                System.out.println(x);
                 JOptionPane.showMessageDialog(null,
-                        "Valid Email",
-                        "Right Email",
-                        JOptionPane.INFORMATION_MESSAGE);
-            } else {
-                JOptionPane.showMessageDialog(null,
-                        "More invalid than Clarita (Heidi's friend)",
+                        "Ha habido algún problema con la conexión.",
                         "Error",
                         JOptionPane.INFORMATION_MESSAGE);
             }
